3b34f6a751a158b5b20b40abdb4c571b927048fa
Binh Pham <thanhbinh0995@gmail.com>
2020-01-08 22:49:50 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { withNamespaces } from 'react-i18next';
+import { hot } from 'react-hot-loader/root';
 import Nav from './pages/common/Nav';
 import Footer from './pages/common/Footer';
 import Routes from './routes';
@@ -25,4 +27,4 @@ AppPage.propTypes = {};
 
 AppPage.defaultProps = {};
 
-export default withRouter(withNamespaces('translations')(AppPage));
+export default hot(withRouter(withNamespaces('translations')(AppPage)));
